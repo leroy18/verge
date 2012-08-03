7b41ac815465c38216dc4f2cfac2210c2d233b83
Lud Gabor <sprinter85@freemail.hu>
2012-08-03 17:24:41 +0200

    couchdb documents can now be created through the signup form using curl

--- index.php
+++ index.php
@@ -17,6 +17,24 @@ post('/signup', function($app) {
     $user->email = $app->form('email');
     echo json_encode($user);
     
+    $curl = curl_init();
+    // curl options
+    // curl options
+    $options = array(
+        CURLOPT_URL => 'http://localhost:5984/verge',
+        CURLOPT_POSTFIELDS => json_encode($user),
+        CURLOPT_HTTPHEADER => array ("Content-Type: application/json"),
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "utf-8",
+        CURLOPT_HEADER => false,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_AUTOREFERER => true
+    );
+    curl_setopt_array($curl, $options);
+    curl_exec($curl);
+    curl_close($curl);
+    
     $app->set('message', 'Thanks for Signing Up ' . $app->form('name') . '!');
     $app->render('home');
 });
