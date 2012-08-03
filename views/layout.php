a329294f875190734a133290a4240885bdc39361
Lud Gabor <sprinter85@freemail.hu>
2012-08-03 14:50:43 +0200

    added clean routes for public files, created a master.css file, linked to master.css in layout.php

--- views/layout.php
+++ views/layout.php
@@ -1,4 +1,7 @@
 <html>
+<head>
+<link href="<?php echo 'http://localhost/couchdb_and_php/verge/css/master.css' ?>" rel="stylesheet" type="text/css" />
+</head>
 <body>
 <h1>Verge</h1>
 <?php include($this->content); ?>
